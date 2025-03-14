75fa2c8e9217d5d92a5986fbd988c9dad3758edb
Ari Wynder <ari.wynder@proton.me>
2025-03-14 07:49:05 -0600

    Cleaned up code and organized methods

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -1,9 +1,7 @@
 package projects;
 
-import projects.dao.DbConnection;
 import projects.exception.DbException;
 
-import java.sql.Connection;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
@@ -15,25 +13,25 @@ import java.util.Scanner;
  */
 public class ProjectsApp {
 
-    private Scanner scanner = new Scanner(System.in);
-    /**
-     * The main method serves as the entry point to the ProjectsApp application.
-     * It initializes a connection to the database by invoking the DbConnection.getConnection() method.
-     *
-     * @param args Command-line arguments passed to the application. These are not used in this implementation.
-     */
-    public static void main(String[] args) {
-        //Get connection
-        Connection connection = DbConnection.getConnection();
-    }
-
     //@formatter:off
-    private List<String> operations = List.of(
+    List<String> operations = List.of(
             "1) Add a project"
     );
     //@formatter:on
 
+    private Scanner scanner = new Scanner(System.in);
 
+
+    /**
+     * The main method serves as the entry point for the ProjectsApp application.
+     * It initializes the application and begins the processing of user selections.
+     *
+     * @param args command-line arguments passed to the application (not used in this implementation)
+     */
+    public static void main(String[] args) {
+        new ProjectsApp().processUserSelections();
+
+    }
 
     private void processUserSelections() {
         boolean done = false;
@@ -57,19 +55,18 @@ public class ProjectsApp {
 
     private void printOperations() {
         System.out.println("\nThese are the available selections. Press the Enter key to quit:");
-        operations.forEach(System.out::println);
     }
 
     private Integer getIntInput(String prompt) {
         String input = getStringInput(prompt);
-        if(Objects. isNull(input)) {
+        if(Objects.isNull(input)) {
             return null;
         }
         try {
             return Integer.valueOf(input);
         }
         catch(NumberFormatException e) {
-            throw new DbException(input + "is not a valid number.");
+            throw new DbException(input + "is not a valid number. Try again.");
         }
     }
 
@@ -77,7 +74,8 @@ public class ProjectsApp {
         System.out.print(prompt + ": ");
         String input = scanner.nextLine();
 
-        return input.isBlank() ? null : input. trim();
+
+        return input.isEmpty() ? null : input. trim();
     }
 }
 
